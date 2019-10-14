f5ff9b3eaea24db43f72ca082ee9436a09862d09
BmeUP <44481957+BmeUP@users.noreply.github.com>
2019-10-14 13:57:46 +0300

    Add files via upload

---
+++ fibo.py
@@ -0,0 +1,23 @@
+import time
+
+
+def fibo(n):
+    a, b = 0, 1
+    fb = [0, 1]
+    if n == 0:
+        print([0])
+    elif n == 1:
+        print([0, 1])
+    elif n == 2:
+        print([0, 1, 1])
+    else:
+        for i in range(2, n):
+            a, b = b, b+a
+            fb.append(b)
+            print(fb)
+            time.sleep(1)
+
+
+n = int(input("number please: "))
+
+fibo(n)
